eed51f65e4ee552912390b0f9f60ba3ee6596b4b
王金宇 <wangjinyu@users.noreply.github.com>
2017-06-28 10:48:48 +0800

    Initial commit

---
+++ decode_xlog/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  decode_xlog
+//
+//  Created by jinyu on 2017/6/28.
+//  Copyright © 2017年 jinyu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ decode_xlog/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  decode_xlog
+//
+//  Created by jinyu on 2017/6/28.
+//  Copyright © 2017年 jinyu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+}
+
